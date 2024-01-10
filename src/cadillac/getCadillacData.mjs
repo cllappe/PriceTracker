ea543e3e609321ef1db5b848e66585dee1dad8cc
Chelsea Lappe <chelsea.lappe@7factor.io>
2024-01-10 12:55:44 -0700

    getting raw cadillac data

---
+++ src/cadillac/getCadillacData.mjs
@@ -0,0 +1,49 @@
+import { firefox } from "playwright";
+import axios from "axios";
+import * as fs from 'node:fs';
+
+let validHeaders = null;
+function interceptRequest(request) {
+  if (request.url().includes("v1/vehicles/search")) {
+    validHeaders = request.headers();
+  }
+  return request;
+}
+
+async function getValidHeaders() {
+  const browser = await firefox.launch();
+  const page = await browser.newPage();
+  page.on("request", interceptRequest);
+  await page.goto(
+    "https://www.cadillac.com/shopping/inventory/search/lyriq/2024?paymentType=CASH&radius=40&sort=distance%2CASC&zipCode=80919"
+  );
+  await page.waitForLoadState("networkidle");
+  await browser.close();
+}
+
+async function getCadillacDataRaw() {
+  await getValidHeaders();
+  let axiosConfig = {
+        headers: validHeaders
+  }
+   axios.post(
+    "https://www.cadillac.com/cadillac/shopping/api/aec-cp-discovery-api/p/v1/vehicles/search",
+    {
+      filters: {
+        year: { values: ["2024"] },
+        model: { values: ["lyriq"] },
+        geo: { zipCode: "80919", radius: 40 },
+      },
+      sort: { name: "distance", order: "ASC" },
+      paymentTypes: ["CASH"],
+      pagination: { size: 99 },
+    },
+    axiosConfig
+  ).then((response) => {
+    fs.writeFile('resultsCadillac.json', JSON.stringify(response.data), function (err) {
+      if (err) throw err;
+    });
+  });
+}
+
+getCadillacDataRaw();
