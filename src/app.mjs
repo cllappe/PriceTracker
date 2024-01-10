e9761db25b929609f43939da613ef184fd669d3e
Chelsea Lappe <chelsea.lappe@7factor.io>
2024-01-10 14:46:43 -0700

    add cron ability

--- src/app.mjs
+++ src/app.mjs
@@ -1,9 +1,13 @@
+import cron from 'node-cron';
+
 import { processToyota } from './toyota/processToyotaData.mjs';
 import { getToyotaDataRaw } from './toyota/getToyotaData.mjs';
 import { processCadillac } from './cadillac/processCadillacData.mjs';
 import { getCadillacDataRaw } from './cadillac/getCadillacData.mjs';
 
-await getToyotaDataRaw();
-await processToyota();
-await getCadillacDataRaw();
-await processCadillac();
+cron.schedule('0 6 * * * *', async () => {
+    await getToyotaDataRaw();
+    await processToyota();
+    await getCadillacDataRaw();
+    await processCadillac();
+});
