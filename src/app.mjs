d09ff84bb87a54872618f619b60ecc473db8135f
Chelsea Lappe <chelsea.lappe@7factor.io>
2024-01-11 09:30:19 -0700

    fixing cron to run at 6 am every day

--- src/app.mjs
+++ src/app.mjs
@@ -5,7 +5,8 @@ import { getToyotaDataRaw } from './toyota/getToyotaData.mjs';
 import { processCadillac } from './cadillac/processCadillacData.mjs';
 import { getCadillacDataRaw } from './cadillac/getCadillacData.mjs';
 
-cron.schedule('0 6 * * * *', async () => {
+cron.schedule('0 0 6 * * *', async () => {
+    console.log('Running task at ', new Date().toLocaleString());
     await getToyotaDataRaw();
     await processToyota();
     await getCadillacDataRaw();
